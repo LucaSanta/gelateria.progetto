050433289cb782faa94bcddc5d461d6f9e484773
Beatrice <beatrice.castaldo@gmail.com>
2016-04-13 20:02:04 +0200

    fullentità

---
+++ src/FrontBundle/Entity/Citta.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace FrontBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Citta
+ *
+ * @ORM\Table(name="citta")
+ * @ORM\Entity(repositoryClass="FrontBundle\Repository\CittaRepository")
+ */
+class Citta
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idProvincia", type="integer")
+     */
+    private $idProvincia;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nome", type="string", length=255)
+     */
+    private $nome;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set idProvincia
+     *
+     * @param integer $idProvincia
+     *
+     * @return Citta
+     */
+    public function setIdProvincia($idProvincia)
+    {
+        $this->idProvincia = $idProvincia;
+
+        return $this;
+    }
+
+    /**
+     * Get idProvincia
+     *
+     * @return int
+     */
+    public function getIdProvincia()
+    {
+        return $this->idProvincia;
+    }
+
+    /**
+     * Set nome
+     *
+     * @param string $nome
+     *
+     * @return Citta
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get nome
+     *
+     * @return string
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+}
+
---
+++ src/FrontBundle/Entity/Ricerca.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace FrontBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ricerca
+ *
+ * @ORM\Table(name="ricerca")
+ * @ORM\Entity(repositoryClass="FrontBundle\Repository\RicercaRepository")
+ */
+class Ricerca
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="data", type="datetime", unique=true)
+     */
+    private $data;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idUser", type="integer")
+     */
+    private $idUser;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idGusto", type="integer")
+     */
+    private $idGusto;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idCitta", type="integer")
+     */
+    private $idCitta;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set data
+     *
+     * @param \DateTime $data
+     *
+     * @return Ricerca
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Get data
+     *
+     * @return \DateTime
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Set idUser
+     *
+     * @param integer $idUser
+     *
+     * @return Ricerca
+     */
+    public function setIdUser($idUser)
+    {
+        $this->idUser = $idUser;
+
+        return $this;
+    }
+
+    /**
+     * Get idUser
+     *
+     * @return int
+     */
+    public function getIdUser()
+    {
+        return $this->idUser;
+    }
+
+    /**
+     * Set idGusto
+     *
+     * @param string $idGusto
+     *
+     * @return Ricerca
+     */
+    public function setIdGusto($idGusto)
+    {
+        $this->idGusto = $idGusto;
+
+        return $this;
+    }
+
+    /**
+     * Get idGusto
+     *
+     * @return string
+     */
+    public function getIdGusto()
+    {
+        return $this->idGusto;
+    }
+
+    /**
+     * Set idCitta
+     *
+     * @param integer $idCitta
+     *
+     * @return Ricerca
+     */
+    public function setIdCitta($idCitta)
+    {
+        $this->idCitta = $idCitta;
+
+        return $this;
+    }
+
+    /**
+     * Get idCitta
+     *
+     * @return int
+     */
+    public function getIdCitta()
+    {
+        return $this->idCitta;
+    }
+}
+
